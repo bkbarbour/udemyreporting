95ee7104d0d9029fd608e0544987375a990ba570
Brian Barbour <bkbarbour@gmail.com>
2024-07-02 16:00:30 -0400

    Working on Json Parser

--- src/main/java/com/bbarb75/udemyreporting/Entities/Employee.java
+++ src/main/java/com/bbarb75/udemyreporting/Entities/Employee.java
@@ -1,8 +1,36 @@
 package com.bbarb75.udemyreporting.Entities;
 
 public class Employee {
-    String firstName;
-    String lastName;
+    String name;
     String lastActivity;
+
+    public Employee(String name, String lastActivity) {
+        this.name = name;
+        this.lastActivity = lastActivity;
+    }
+
+    public Employee(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(String lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+
+
+
 
 }
--- src/main/java/com/bbarb75/udemyreporting/Services/EmployeeService.java
+++ src/main/java/com/bbarb75/udemyreporting/Services/EmployeeService.java
@@ -1,8 +1,14 @@
 package com.bbarb75.udemyreporting.Services;
 
 import com.bbarb75.udemyreporting.API.ApiClient;
+import com.bbarb75.udemyreporting.Entities.Employee;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -11,5 +17,33 @@ public class EmployeeService {
     @Autowired
     public EmployeeService(ApiClient apiClient){
         this.apiClient = apiClient;
+    }
+
+    public List<Employee> parseApiResponse(String jsonResponse){
+        List<Employee> employees = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode responseNode = mapper.readTree(jsonResponse).get("response");
+
+            if (responseNode !=null){
+                for (JsonNode employeeNode : responseNode){
+                    JsonNode employeeFirstNameNode = employeeNode.get("user_name");
+                    JsonNode employeeLastNameNode = employeeNode.get("user_surname");
+                    JsonNode employeeCourseAccessedNode = employeeNode.get("course_last_accessed_date");
+
+                    String name = employeeFirstNameNode.toString() + " " + employeeLastNameNode.toString();
+                    String courseLastAccessed = employeeCourseAccessedNode.toString();
+
+                    Employee employee = new Employee();
+                    employee.setName(name);
+                    employee.setLastActivity(courseLastAccessed);
+
+                    employees.add(employee);
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } return employees;
     }
 }
