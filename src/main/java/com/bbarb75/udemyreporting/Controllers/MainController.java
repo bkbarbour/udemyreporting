53bf8502685d1383e4c23025c437ee1fe72e818e
Brian Barbour <bkbarbour@gmail.com>
2024-09-30 16:12:00 -0400

    Modifying and undoing some html changes to add selectors for employee data, need to add some additional functions to handle data retrieval for specific employees tomorrow

--- src/main/java/com/bbarb75/udemyreporting/Controllers/MainController.java
+++ src/main/java/com/bbarb75/udemyreporting/Controllers/MainController.java
@@ -5,6 +5,7 @@ import com.bbarb75.udemyreporting.Services.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,10 +16,11 @@ public class MainController {
     public MainController(EmployeeService employeeService) {this.employeeService = employeeService;}
 
     @GetMapping("/home")
-    public String getEmployee(Model model) {
+    public String getEmployee(@RequestParam(value = "employeeName", defaultValue = "select")String employeeName, Model model) {
         //String jsonResponse = employeeService.fetchEmployeeData();
         List<Employee> employees = employeeService.fetchAllEmployees(); //this is the issue - all data is being added but this is grabbing it before
         model.addAttribute("employees", employees);
+        model.addAttribute("employeeName", employeeName);
         return "home";
     }
 }
